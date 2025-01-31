e33d9ad4470af4f9ab49e29938a7af16d997e41b
Fabián balmaceda rescia <fabianbalmaceda2@gmail.com>
2025-01-30 23:15:31 -0600

    Refactor environment variable access and add console log for debugging

--- src/server/global_variables.ts
+++ src/server/global_variables.ts
@@ -1,9 +1,10 @@
 
 
+console.log('Hello, World!', process.env);
 export const ENV = {
     PORT: (process.env.PORT || 3000) as number,
     HOST: process.env.HOST || 'localhost',
-    PROD_HOST: process.env.PROD_HOST || 'http://localhost:3000',
+    PROD_HOST: process.env['PROD_HOST'] || 'http://localhost:3000',
     API_KEYS: (process.env.API_KEYS || 'secure_api_key,change_me').split(','),
     server_isHealthy: false,
     server_isReady: false,
