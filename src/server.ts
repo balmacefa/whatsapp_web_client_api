753fdfe4562dd297784136b5f3997ae83adf8757
Fabián balmaceda rescia <fabianbalmaceda2@gmail.com>
2025-01-31 00:13:57 -0600

    Enhance homepage to display API version and improve layout with Bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import cors from 'cors';
 import express from 'express';
+import { readFileSync } from "fs";
+import path from 'path';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { defineRoutes } from './api/routes';
 import { ENV } from './server/global_variables';
 
-console.log('ENV.PROD_HOST!', ENV.PROD_HOST);
+
+const executionPath = process.cwd();
+const packageJsonPath = path.join(executionPath, 'package.json');
+const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
 
 const swaggerOptions: swaggerJSDoc.Options = {
     definition: {
@@ -39,7 +44,8 @@ export const createApp = async () => {
 
 
     app.get('/', (req, res) => {
-        res.send(`
+        res.send(
+            `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -50,21 +56,31 @@ export const createApp = async () => {
         </head>
         <body class="bg-light">
             <div class="container text-center mt-5">
-                <h1 class="mb-4">Welcome to the WhatsApp API</h1>
-                <p class="lead">This API provides several functionalities to manage WhatsApp clients and messages:</p>
-                <ul class="list-group list-group-flush text-start d-inline-block">
-                    <li class="list-group-item"><strong>Client Management:</strong> Create, list, delete, and update clients.</li>
-                    <li class="list-group-item"><strong>Messaging:</strong> Send text and media messages.</li>
-                    <li class="list-group-item"><strong>QR Code:</strong> Retrieve QR codes for authentication.</li>
-                    <li class="list-group-item"><strong>Contacts:</strong> Fetch contact lists.</li>
-                    <li class="list-group-item"><strong>Chats:</strong> Retrieve chat history.</li>
-                </ul>
-                <a href="/swagger" class="btn btn-primary mt-4">Go to API Docs</a>
+            <div class="row">
+                <div class="col">
+                    <h1 class="mb-4">Welcome to the WhatsApp API</h1>
+                    <p class="lead">Version: ${packageJson.version}</p>
+                    <p class="lead">This API provides several functionalities to manage WhatsApp clients and messages:</p>
+                    <ul class="list-group list-group-flush text-start d-inline-block">
+                        <li class="list-group-item"><strong>Client Management:</strong> Create, list, delete, and update clients.</li>
+                        <li class="list-group-item"><strong>Messaging:</strong> Send text and media messages.</li>
+                        <li class="list-group-item"><strong>QR Code:</strong> Retrieve QR codes for authentication.</li>
+                        <li class="list-group-item"><strong>Contacts:</strong> Fetch contact lists.</li>
+                        <li class="list-group-item"><strong>Chats:</strong> Retrieve chat history.</li>
+                    </ul>
+                </div>
+                    
+                <div class="col">
+                    <h2 class="mt-4">API Documentation</h2>
+                    <p class="lead">You can find the API documentation below:</p>
+                    <a href="/swagger" class="btn btn-primary mt-4">Go to Swagger UI</a>
+                </div>
+            </div>
             </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         </body>
         </html>
-    `);
+        `);
     });
 
 
